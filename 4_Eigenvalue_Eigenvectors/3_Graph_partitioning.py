f9322d98e086724224ab9e5440cf6a22a69a6e04
Dario <dario.rodriguez9378@hotmail.com>
2024-03-19 00:13:52 -0500

    Graph partitioning using eigenvalues example fully implemented

--- 4_Eigenvalue_Eigenvectors/3_Graph_partitioning.py
+++ 4_Eigenvalue_Eigenvectors/3_Graph_partitioning.py
@@ -1,6 +1,15 @@
+__author__ = 'Dario Rodriguez'
+
 import numpy as np
 import scipy.sparse as ss
+import numpy.linalg as la
 import matplotlib.pyplot as plt
+
+"""
+we consider the Fiedler algorithm, which partitions graphs by finding eigenvectors of a matrix 
+associated with the graph. The Lanczos algorithm was implemented to find a particular eigenvector.
+"""
+
 
 # def open(filename, mode="r"):
 #     """
@@ -24,6 +33,7 @@ def readmesh(fname):
     and `ntriangles` is the number of triangles of the
     mesh.
     """
+    fname = 'data/' + fname
     with open(fname, "r") as f:
         npoints = int(next(f))
         points = np.zeros((npoints, 2))
@@ -77,11 +87,55 @@ def mesh2dualgraph(triangles):
     return ss.csr_matrix(G)
 
 def lanczos(L, x0, niter):
-    #Implement the body of this function
+    """
+    Compute the Lanczos algorithm for a given matrix L and initial vector x0.
+    Note: L is sparse matrix but symmetric.
+    """
+    n, m = L.shape
+    Q = np.zeros((n, niter + 1))
+    T = np.zeros((niter, niter))
+
+    Q[:, 0] = np.zeros(n)
+    Q[:, 1] = x0 / np.linalg.norm(x0)
+    beta_km1 = 0
+
+    for k in range(1, niter):
+        u_k = L @ Q[:, k]
+        alpha_k = np.dot(Q[:, k], u_k)
+        u_k = u_k - alpha_k * Q[:, k] - beta_km1 * Q[:, k - 1]
+        beta_k = np.linalg.norm(u_k)
+
+        if (beta_k == 0):
+            print("Lanczos algorithm terminated at iteration ", k, " because beta_k = 0")
+            break
+
+        Q[:, k + 1] = u_k / beta_k
+
+        T[k - 1, k - 1] = alpha_k
+        T[k, k - 1] = beta_k
+        T[k - 1, k] = beta_k
+
+        beta_km1 = beta_k
+    T[-1, -1] = Q[:, -1].T @ L @ Q[:, -1]
+    Q = Q[:, 1:]
+
+    # ---------------------------------------------
+    # # Check orthogonality correct
+    # --------------------------------------------
+    # L_matrix = L.toarray()
+    # print(la.norm(Q.T@L_matrix@Q - T) / la.norm(L_matrix))
+    # print(la.norm(Q.T@Q - np.eye(niter)))
+
     return Q, T
 
-def fiedler_ritz(Q,T):
-    #Implement the body of this function
+def fiedler_ritz(Q, T):
+    """ Compute the Fiedler vector using the Ritz vector from returned tri-diagonal matrix T. and the Arnoldi basis Q."""
+    w, v = np.linalg.eig(T)
+    sorted_idx = np.argsort(w)
+    w_min = w[sorted_idx[1]]  # Select second smallest eigenvalue
+    v_min = v[:, sorted_idx[1]]  # Select corresponding eigenvector
+    fiedlerVec = Q @ v_min  # Compute the Ritz vector (Fiedler vector)
+
     return fiedlerVec
 
 def fiedler(G, k):
@@ -95,11 +149,8 @@ def fiedler(G, k):
 
     x0 = np.linspace(1, n, num = n)
 
-    ## You should complete this Lanczos function
     Q, T = lanczos(G, x0, k)
-
-    ## You should complete this Fiedler vector computation function
-    fiedlerVec = fiedler_ritz(Q,T)
+    fiedlerVec = fiedler_ritz(Q, T)
 
     partitionVec = np.zeros_like(fiedlerVec)
     mfiedler = np.ma.median(fiedlerVec)
@@ -115,14 +166,15 @@ def fiedler(G, k):
 
 if __name__ == "__main__":
 
-
+    # Change to mesh.2 to see the partitioning of the graph
     points, triangles = readmesh("mesh.1")
 
-    # Comment this out once you enable the result plotting below.
     plotmesh(points, triangles)
+    plt.show()
 
+    # Generate the Laplacian of the dual graph of G
     G = mesh2dualgraph(triangles)
+
     partitionVec = fiedler(G, 150)
 
-    # Uncomment to see your computed partition!
-    # plotmesh(points, triangles, partitionVec)
+    plotmesh(points, triangles, partitionVec)
